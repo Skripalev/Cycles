68b0d44dee77d1b1ab00743bb23899900a9b701d
Александр <klerik.af.miass@gmail.com>
2024-12-17 20:50:40 +0500

    Home-work.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Проверка связи.");
+        System.out.println("Task 1");
+        for (int i = 0; i <= 10; i= i+1){
+            System.out.println("Число "+ i);}
+
+        System.out.println("Task 2");
+        for ( int i = 10; i >= 0; i = i-1){
+            System.out.println("Число "+i);}
+
+        System.out.println("Task 3");
+        for ( int i = 0; i <= 17; i = i +2){
+            System.out.println("Число "+ i);}
+
+        System.out.println("Task 4");
+        for ( int i = 10; i >= -10; i -- ){
+            System.out.println("Число "+ i);}
+
+        System.out.println("Task 5");
+        for ( int i = 1904; i <= 2096; i= i + 4 ){
+            System.out.println(i+" год является високосным.");}
+
+        System.out.println("Task 6");
+        for ( int i = 7; i <= 98; i = i+7){
+            System.out.println(i);}
+
+        System.out.println("Task 7");
+        for ( int i = 1; i <= 512; i = i * 2){
+            System.out.println(i);}
+
+        System.out.println("Task 8");
+        int salary = 2990;
+        int total = 0;
+        for ( int i = 1; i < 13; i = i + 1 ){
+            total= salary + total;
+            System.out.println("Месяц "+i+", сумма накоплений равна "+total+" рублей.");}
+
+        System.out.println("Task 9");
+        for ( int i = 1; i < 13; i = i + 1 ){
+            total= total + total/100;
+            total= salary + total;
+            System.out.println("Месяц "+i+", сумма накоплений равна "+total+" рублей.");}
+
+        System.out.println("Task 10");
+        int number = 2;
+        System.out.println("Таблица умножения на "+number+":");
+        for ( int i = 1; i <= 10; i ++ ){
+            int result = number * i;
+            System.out.println(number+"x"+i+"="+result);
+        }
+
+
+
+
+
+
+
     }
 }
