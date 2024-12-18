c8a96b3bdeca2c6a45d8d7e79ad06a7cac816d92
Александр <klerik.af.miass@gmail.com>
2024-12-18 07:27:41 +0500

    corrected home-work.

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,30 @@ public class Main {
         for (int i = 0; i <= 10; i = i + 1) {
             System.out.println("Число " + i);
         }
-
         System.out.println("Task 2");
         for (int i = 10; i >= 0; i = i - 1) {
             System.out.println("Число " + i);
         }
-
         System.out.println("Task 3");
         for (int i = 0; i <= 17; i = i + 2) {
             System.out.println("Число " + i);
         }
-
         System.out.println("Task 4");
         for (int i = 10; i >= -10; i--) {
             System.out.println("Число " + i);
         }
-
         System.out.println("Task 5");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным.");
         }
-
         System.out.println("Task 6");
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
-
         System.out.println("Task 7");
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
-
         System.out.println("Task 8");
         int salary = 29000;
         int total = 0;
@@ -42,7 +35,6 @@ public class Main {
             total = salary + total;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
         }
-
         System.out.println("Task 9");
         int salaryTwo = 29000;
         int totalSalary = 0;
@@ -51,7 +43,6 @@ public class Main {
             totalSalary = salaryTwo + totalSalary;
             System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSalary + " рублей.");
         }
-
         System.out.println("Task 10");
         int number = 2;
         System.out.println("Таблица умножения на " + number + ":");
